6e1e1a753c76909758a73336f03b4a13f5393e6a
ckttkjack00 <150279510+ckttkjack00@users.noreply.github.com>
2024-08-30 09:23:03 -0400

    Add files via upload

---
+++ Case Study.py
@@ -0,0 +1,37 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/khaen/AppData/Local/Programs/Python/Python311/Case Study.py
+Enter last name (or 'ZZZ' to quit): Jackson
+Enter first name: Kendal
+Enter GPA: 3.2
+Enter last name (or 'ZZZ' to quit): Jackson
+Enter first name: Charlee
+Enter GPA: 4.0
+Charlee Jackson made the Dean's List.
+Enter last name (or 'ZZZ' to quit): Jackson
+Enter first name: Kacyn
+Enter GPA: 3.5
+Kacyn Jackson made the Dean's List.
+Enter last name (or 'ZZZ' to quit): Jackson
+Enter first name: Teigan
+Enter GPA: 3.7
+Teigan Jackson made the Dean's List.
+Enter last name (or 'ZZZ' to quit): Jackson
+Enter first name: Tatum
+Enter GPA: 3.8
+Tatum Jackson made the Dean's List.
+Enter last name (or 'ZZZ' to quit): Jackson
+Enter first name: Kyndyl
+Enter GPA: 3.8
+Kyndyl Jackson made the Dean's List.
+Enter last name (or 'ZZZ' to quit): Jackson
+Enter first name: Marla
+Enter GPA: 2.3
+Enter last name (or 'ZZZ' to quit): zzz
+>>> 
+= RESTART: C:/Users/khaen/AppData/Local/Programs/Python/Python311/Case Study.py
+Enter last name (or 'ZZZ' to quit): jackson
+Enter first name: kendal
+Enter GPA: 3.25
+kendal jackson made the Honor Roll.
